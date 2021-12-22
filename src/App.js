b6ba12f9a45031c90b6706713a003624193730e0
葫芦岛吴奇隆 <1151480656@qq.com>
2021-12-22 17:15:16 +0800

    1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 改
         </a>
       </header>
     </div>
